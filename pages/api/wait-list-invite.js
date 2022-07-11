820ff2db7c97f93cd3d610104fc49d9545bb8e3e
marcelovicentegc <marcelovicentegc@gmail.com>
2022-07-10 21:22:12 -0300

    chore: move clients to lib folder

--- pages/api/wait-list-invite.js
+++ pages/api/wait-list-invite.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { sendEmail } from "../../clients/mailgun";
+import { sendEmail } from "../../lib/clients/mailgun";
 import {
   BAD_REQUEST,
   METHOD_NOT_ALLOWED,
