b2ca921ccce22dec2473c00973d7f264fb7b3e7b
marcelovicentegc <marcelovicentegc@gmail.com>
2022-08-14 15:23:17 -0300

    feat: custom verify request page and sign in email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,33 +5,19 @@ import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 import { createTransport } from "nodemailer";
+import { html, BRAND_HEX } from "../../../lib/email";
 
 async function sendVerificationRequest(params) {
-  console.log({ params });
-
   const { identifier, url, provider } = params;
   const { host } = new URL(url);
-  // NOTE: You are not required to use `nodemailer`, use whatever you want.
   const transport = createTransport(provider.server);
-
-  console.log({ host, url, provider });
-
-  // FROM MAILGUN.COM
-  // const transport = createTransport({
-  //   host: "smtp.mailgun.org",
-  //   port: 587,
-  //   auth: {
-  //     user: "postmaster@eutiveumsonho.com",
-  //     pass: "YOUR_SMTP_PASSWORD",
-  //   },
-  // });
 
   const result = await transport.sendMail({
     to: identifier,
     from: provider.from,
-    subject: `Entre em ${host}`,
+    subject: `Entrar no Eu tive um sonho`,
     text: text({ url, host }),
-    html: html({ url, host }),
+    html: formatMagicLinkHtml({ url, host }),
   });
 
   const failed = result.rejected.concat(result.pending).filter(Boolean);
@@ -39,69 +25,32 @@ async function sendVerificationRequest(params) {
   if (failed.length) {
     throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
   }
-
-  console.log({ result });
 }
 
-/**
- * Email HTML body
- * Insert invisible space into domains from being turned into a hyperlink by email
- * clients like Outlook and Apple mail, as this is confusing because it seems
- * like they are supposed to click on it to sign in.
- *
- * @note We don't add the email address to avoid needing to escape it, if you do, remember to sanitize it!
- */
-function html(params) {
+function formatMagicLinkHtml(params) {
   const { url, host } = params;
 
-  const escapedHost = host.replace(/\./g, "&#8203;.");
-
-  const brandColor = "#346df1";
-  const color = {
-    background: "#f9f9f9",
-    text: "#444",
-    mainBackground: "#fff",
-    buttonBackground: brandColor,
-    buttonBorder: brandColor,
-    buttonText: "#fff",
-  };
-
-  return `
-<body style="background: ${color.background};">
-  <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
-    <tr>
-      <td align="center"
-        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        Sign in to <strong>${escapedHost}</strong>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 20px 0;">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
-                target="_blank"
-                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
-                in</a></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center"
-        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        If you did not request this email you can safely ignore it.
-      </td>
-    </tr>
-  </table>
-</body>
-`;
+  return html(
+    `<p>Aqui está um link para entrar no Eu tive um sonho. Esse link só pode ser utilizado uma vez e expira depois de 24 horas. Caso o link tenha expirado, por favor tente entrar novamente, <a href=${host}>clicando aqui</a></p>
+<tr>
+  <td align="center" style="padding: 20px 0;">
+    <table border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td align="center" style="border-radius: 5px;" bgcolor="${BRAND_HEX}"><a href="${url}"
+            target="_blank"
+            style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: white; text-decoration: none; border-radius: 5px; padding: 10px 20px; display: inline-block; font-weight: bold;">Entrar agora</a></td>
+      </tr>
+    </table>
+  </td>
+</tr>`,
+    "Entrar no Eu tive um sonho",
+    "<p>Se você não solicitou este e-mail, pode ignorá-lo.</p>"
+  );
 }
 
 /** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
 function text({ url, host }) {
-  return `Sign in to ${host}\n${url}\n\n`;
+  return `Entre em ${host}\n${url}\n\n`;
 }
 
 export const authOptions = {
@@ -118,9 +67,7 @@ export const authOptions = {
         },
       },
       from: process.env.EMAIL_FROM,
-      // sendVerificationRequest(params) {
-      //   return sendVerificationRequest(params);
-      // },
+      sendVerificationRequest,
     }),
     FacebookProvider({
       clientId: process.env.AUTH_FACEBOOK_ID,
@@ -135,7 +82,7 @@ export const authOptions = {
     signIn: "/auth/signin",
     // signOut: '/auth/signout',
     // error: '/auth/error', // Error code passed in query string as ?error=
-    // verifyRequest: '/auth/verify-request', // (used for check email message)
+    verifyRequest: "/auth/verify-request", // (used for check email message)
     // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   },
 };
