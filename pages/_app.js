e43f77366cf02323eb20893164e97b6abcc56601
marcelovicentegc <marcelovicentegc@gmail.com>
2023-08-22 14:53:23 +0200

    chore: add location=yes window feature

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   useEffect(() => {
     if (typeof window !== "undefined") {
       if (navigator.userAgent.includes("Instagram")) {
-        window.open("https://eutiveumsonho.com", "_system");
+        window.open("https://eutiveumsonho.com", "_system", "location=yes");
       }
     }
   }, []);
