323a04abb27553326064c7dcf26fbc6d2ed10f8c
marcelovicentegc <marcelovicentegc@gmail.com>
2024-02-17 19:17:43 -0300

    chore: rename backfilling module

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-import "../lib/utils/development";
+import "../lib/backfilling";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
